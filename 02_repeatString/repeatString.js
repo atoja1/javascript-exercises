4bf38e2277ea2b35cbab4b487d1c54b5e91f1c57
atoja <miloszszymczak11@gmail.com>
2024-09-10 14:03:26 +0200

    complete exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,18 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  
+  if (num >= 0)
+  {
+    var endString = ""
+    for (let i = 1; i <= num; i++)
+    {
+        endString += string;
+    }
+    return endString;
+  }
+  else 
+  {
+    return "ERROR";
+  }
 
 };
 
