d258cac72bef1e53ba3473cb9f9682320978baf5
atoja <miloszszymczak11@gmail.com>
2024-09-10 14:39:09 +0200

    complete exercise 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const stringArray = [];
+    for (i = 0; i <= string.length; i++) {
+        stringArray[i] = string.at(i)       
+    }
+    const reversed = stringArray.reverse();
 
+    result = reversed.join("");
+    
+    return result;
 };
 
 // Do not edit below this line
