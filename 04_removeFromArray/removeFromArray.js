eb3f2b48e20128b760925dbbd48ff3246470600b
atoja <miloszszymczak11@gmail.com>
2024-09-11 11:31:26 +0200

    complete exercise 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(removeFrom, ...removeItems) {
+    for (const item of removeItems) {
+        while (removeFrom.includes(item)) {
+            if (removeFrom.includes(item)){
+            removeFrom.splice(removeFrom.indexOf(item),1)
+            console.log(removeFrom)
+            }
+        }    
+        
+    }
+    return removeFrom
 };
 
 // Do not edit below this line
