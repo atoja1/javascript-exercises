cd2895532e158ed0960d23b4f90d420c2b1f828b
atoja <miloszszymczak11@gmail.com>
2024-09-11 16:12:43 +0200

    complete exercise 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemperature) {
+  return Math.round(((fahrenheitTemperature-32)/1.8)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemperature) {
+  return Math.round(((celsiusTemperature*1.8)+32)*10)/10
 };
 
 // Do not edit below this line
