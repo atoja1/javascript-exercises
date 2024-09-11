71bd3a03f55971b89c7de2b1042914a9e61e17fd
atoja <miloszszymczak11@gmail.com>
2024-09-11 15:26:08 +0200

    complete exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    let sum;
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+        var newMax = min;
+        return (newMax/2)*(max+newMax)
+    }
+    return (max/2)*(min+max)
 };
 
 // Do not edit below this line
