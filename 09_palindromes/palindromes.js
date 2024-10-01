4f57c77156e631e793fc6ca5eecaab369eba24b8
atoja <miloszszymczak11@gmail.com>
2024-10-01 19:59:50 +0200

    complete exercise 09-palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const newString = string
+    .toLowerCase()
+    .split("")
+    .reverse()
+    .filter(char => {
+        return /[a-z0-9]/.test(char)
+    })
+    .join("")
+    
+    const oldString = string
+    .toLowerCase()
+    .split("")
+    .filter(char => {
+        return /[a-z0-9]/.test(char)
+    })
+    .join("")
+    return oldString == newString    
 };
 
 // Do not edit below this line
