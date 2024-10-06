105bb3860147e4a90401ac35655e9be6859dcc20
atoja <miloszszymczak11@gmail.com>
2024-10-06 17:18:03 +0200

    complete exercise 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    if (number === 0 || number === "0") return 0;
+    if (number <= 0) return "OOPS";
 
+    let n1 = 1, n2 = 1, sum;
+    for (let i = 1; i < number; i++) {
+        sum = n1 + n2;
+        n1 = n2;
+        n2 = sum;
+    }
+    return(n1);
 };
+fibonacci(20)
 
 // Do not edit below this line
 module.exports = fibonacci;
