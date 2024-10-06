0937b0960bf897f35c9279c5d303e1df54a9daab
atoja <miloszszymczak11@gmail.com>
2024-10-07 00:20:50 +0200

    complete exercise 11_getTheTtitle and 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
+
+
+const findTheOldest = function(people) {
+    // console.table(people)
+    const people2 = people
+        .map((person) => {
+            if (person.yearOfDeath === undefined) person.yearOfDeath = new Date().getFullYear();
+            age = person.yearOfDeath - person.yearOfBirth
+            return {name: person.name, age}
+        })
+        .sort((a,b) => a.age - b.age)
+    console.table(people2)
+    return(people2.at(-1))
 };
-
+findTheOldest(people)
 // Do not edit below this line
 module.exports = findTheOldest;
